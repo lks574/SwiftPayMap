15d1c97dad5c65d6d11cfd55ab77a85fc079ceca
이경석 <jack.lee@surespeaking.com>
2019-03-04 15:48:38 +0900

    Initial Commit

---
+++ SwiftPayMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftPayMap
+//
+//  Created by KyungSeok Lee on 2019. 3. 4..
+//  Copyright © 2019년 KyungSeok Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
